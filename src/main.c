3f4772a0b7250b1f8f5eca09ec78fda7272ecf0f
Adam Magaluk <adammagaluk@gmail.com>
2012-07-29 22:15:15 -0400

    Removed test protocols

--- src/main.c
+++ src/main.c
@@ -49,8 +49,7 @@ enum demo_protocols {
 	/* always first */
 	PROTOCOL_HTTP = 0,
 
-	PROTOCOL_DUMB_INCREMENT,
-	PROTOCOL_LWS_MIRROR,
+	PROTOCOL_COMMOTION_WS,
 
 	/* always last */
 	DEMO_PROTOCOL_COUNT
@@ -71,31 +70,7 @@ static int callback_http(struct libwebsocket_context *context,
 	char client_ip[128];
 
 	switch (reason) {
-	case LWS_CALLBACK_HTTP:
-		fprintf(stderr, "serving HTTP URI %s\n", (char *)in);
-
-		if (in && strcmp(in, "/favicon.ico") == 0) {
-			if (libwebsockets_serve_http_file(wsi,
-			     "favicon.ico", "image/x-icon"))
-				fprintf(stderr, "Failed to send favicon\n");
-			break;
-		}
-
-		/* send the script... when it runs it'll start websockets */
-
-		if (libwebsockets_serve_http_file(wsi,
-				  "test.html", "text/html"))
-			fprintf(stderr, "Failed to send HTTP file\n");
-		break;
-
-	/*
-	 * callback for confirming to continue with client IP appear in
-	 * protocol 0 callback since no websocket protocol has been agreed
-	 * yet.  You can just ignore this if you won't filter on client IP
-	 * since the default uhandled callback return is 0 meaning let the
-	 * connection continue.
-	 */
-
+	case LWS_CALLBACK_HTTP: break;
 	case LWS_CALLBACK_FILTER_NETWORK_CONNECTION:
 
 		libwebsockets_get_peer_addresses((int)(long)user, client_name,
@@ -106,7 +81,6 @@ static int callback_http(struct libwebsocket_context *context,
 
 		/* if we returned non-zero from here, we kill the connection */
 		break;
-
 	default:
 		break;
 	}
@@ -170,187 +144,72 @@ dump_handshake_info(struct lws_tokens *lwst)
  * connection.
  */
 
-struct per_session_data__dumb_increment {
+struct per_session_data__ws_client{
 	int number;
 };
 
-static int
-callback_dumb_increment(struct libwebsocket_context *context,
+static int commotion_ws_callback(struct libwebsocket_context *context,
 			struct libwebsocket *wsi,
 			enum libwebsocket_callback_reasons reason,
 					       void *user, void *in, size_t len)
 {
-	int n;
-	unsigned char buf[LWS_SEND_BUFFER_PRE_PADDING + 512 +
-						  LWS_SEND_BUFFER_POST_PADDING];
-	unsigned char *p = &buf[LWS_SEND_BUFFER_PRE_PADDING];
-	struct per_session_data__dumb_increment *pss = user;
+    int n;
+    unsigned char buf[LWS_SEND_BUFFER_PRE_PADDING + 512 + LWS_SEND_BUFFER_POST_PADDING];
+    unsigned char *p = &buf[LWS_SEND_BUFFER_PRE_PADDING];
+    struct per_session_data__ws_client *pss = user;
 
-	switch (reason) {
+    switch (reason) {
 
-	case LWS_CALLBACK_ESTABLISHED:
-		fprintf(stderr, "callback_dumb_increment: "
-						 "LWS_CALLBACK_ESTABLISHED\n");
-		pss->number = 0;
-		break;
+        case LWS_CALLBACK_ESTABLISHED:
+            fprintf(stderr, "callback_dumb_increment: LWS_CALLBACK_ESTABLISHED\n");
+            pss->number = 0;
+            break;
 
-	/*
-	 * in this protocol, we just use the broadcast action as the chance to
-	 * send our own connection-specific data and ignore the broadcast info
-	 * that is available in the 'in' parameter
-	 */
+            /*
+             * in this protocol, we just use the broadcast action as the chance to
+             * send our own connection-specific data and ignore the broadcast info
+             * that is available in the 'in' parameter
+             */
 
-	case LWS_CALLBACK_BROADCAST:
-		n = sprintf((char *)p, "%d", pss->number++);
-		n = libwebsocket_write(wsi, p, n, LWS_WRITE_TEXT);
-		if (n < 0) {
-			fprintf(stderr, "ERROR writing to socket");
-			return 1;
-		}
-		if (close_testing && pss->number == 50) {
-			fprintf(stderr, "close tesing limit, closing\n");
-			libwebsocket_close_and_free_session(context, wsi,
-						       LWS_CLOSE_STATUS_NORMAL);
-		}
-		break;
+        case LWS_CALLBACK_BROADCAST:
+            n = sprintf((char *) p, "%d", pss->number++);
+            n = libwebsocket_write(wsi, p, n, LWS_WRITE_TEXT);
+            if (n < 0) {
+                fprintf(stderr, "ERROR writing to socket");
+                return 1;
+            }
+            if (close_testing && pss->number == 50) {
+                fprintf(stderr, "close tesing limit, closing\n");
+                libwebsocket_close_and_free_session(context, wsi,
+                        LWS_CLOSE_STATUS_NORMAL);
+            }
+            break;
 
-	case LWS_CALLBACK_RECEIVE:
-		fprintf(stderr, "rx %d\n", (int)len);
-		if (len < 6)
-			break;
-		if (strcmp(in, "reset\n") == 0)
-			pss->number = 0;
-		break;
-	/*
-	 * this just demonstrates how to use the protocol filter. If you won't
-	 * study and reject connections based on header content, you don't need
-	 * to handle this callback
-	 */
+        case LWS_CALLBACK_RECEIVE:
+            fprintf(stderr, "rx %d\n", (int) len);
+            if (len < 6)
+                break;
+            if (strcmp(in, "reset\n") == 0)
+                pss->number = 0;
+            break;
+            /*
+             * this just demonstrates how to use the protocol filter. If you won't
+             * study and reject connections based on header content, you don't need
+             * to handle this callback
+             */
 
-	case LWS_CALLBACK_FILTER_PROTOCOL_CONNECTION:
-		dump_handshake_info((struct lws_tokens *)(long)user);
-		/* you could return non-zero here and kill the connection */
-		break;
+        case LWS_CALLBACK_FILTER_PROTOCOL_CONNECTION:
+            dump_handshake_info((struct lws_tokens *) (long) user);
+            /* you could return non-zero here and kill the connection */
+            break;
 
-	default:
-		break;
-	}
+        default:
+            break;
+    }
 
-	return 0;
+    return 0;
 }
 
-
-/* lws-mirror_protocol */
-
-#define MAX_MESSAGE_QUEUE 64
-
-struct per_session_data__lws_mirror {
-	struct libwebsocket *wsi;
-	int ringbuffer_tail;
-};
-
-struct a_message {
-	void *payload;
-	size_t len;
-};
-
-static struct a_message ringbuffer[MAX_MESSAGE_QUEUE];
-static int ringbuffer_head;
-
-
-static int
-callback_lws_mirror(struct libwebsocket_context *context,
-			struct libwebsocket *wsi,
-			enum libwebsocket_callback_reasons reason,
-					       void *user, void *in, size_t len)
-{
-	int n;
-	struct per_session_data__lws_mirror *pss = user;
-
-	switch (reason) {
-
-	case LWS_CALLBACK_ESTABLISHED:
-		fprintf(stderr, "callback_lws_mirror: "
-						 "LWS_CALLBACK_ESTABLISHED\n");
-		pss->ringbuffer_tail = ringbuffer_head;
-		pss->wsi = wsi;
-		break;
-
-	case LWS_CALLBACK_SERVER_WRITEABLE:
-		if (close_testing)
-			break;
-		if (pss->ringbuffer_tail != ringbuffer_head) {
-
-			n = libwebsocket_write(wsi, (unsigned char *)
-				   ringbuffer[pss->ringbuffer_tail].payload +
-				   LWS_SEND_BUFFER_PRE_PADDING,
-				   ringbuffer[pss->ringbuffer_tail].len,
-								LWS_WRITE_TEXT);
-			if (n < 0) {
-				fprintf(stderr, "ERROR writing to socket");
-				exit(1);
-			}
-
-			if (pss->ringbuffer_tail == (MAX_MESSAGE_QUEUE - 1))
-				pss->ringbuffer_tail = 0;
-			else
-				pss->ringbuffer_tail++;
-
-			if (((ringbuffer_head - pss->ringbuffer_tail) %
-				  MAX_MESSAGE_QUEUE) < (MAX_MESSAGE_QUEUE - 15))
-				libwebsocket_rx_flow_control(wsi, 1);
-
-			libwebsocket_callback_on_writable(context, wsi);
-
-		}
-		break;
-
-	case LWS_CALLBACK_BROADCAST:
-		n = libwebsocket_write(wsi, in, len, LWS_WRITE_TEXT);
-		if (n < 0)
-			fprintf(stderr, "mirror write failed\n");
-		break;
-
-	case LWS_CALLBACK_RECEIVE:
-
-		if (ringbuffer[ringbuffer_head].payload)
-			free(ringbuffer[ringbuffer_head].payload);
-
-		ringbuffer[ringbuffer_head].payload =
-				malloc(LWS_SEND_BUFFER_PRE_PADDING + len +
-						  LWS_SEND_BUFFER_POST_PADDING);
-		ringbuffer[ringbuffer_head].len = len;
-		memcpy((char *)ringbuffer[ringbuffer_head].payload +
-					  LWS_SEND_BUFFER_PRE_PADDING, in, len);
-		if (ringbuffer_head == (MAX_MESSAGE_QUEUE - 1))
-			ringbuffer_head = 0;
-		else
-			ringbuffer_head++;
-
-		if (((ringbuffer_head - pss->ringbuffer_tail) %
-				  MAX_MESSAGE_QUEUE) > (MAX_MESSAGE_QUEUE - 10))
-			libwebsocket_rx_flow_control(wsi, 0);
-
-		libwebsocket_callback_on_writable_all_protocol(
-					       libwebsockets_get_protocol(wsi));
-		break;
-	/*
-	 * this just demonstrates how to use the protocol filter. If you won't
-	 * study and reject connections based on header content, you don't need
-	 * to handle this callback
-	 */
-
-	case LWS_CALLBACK_FILTER_PROTOCOL_CONNECTION:
-		dump_handshake_info((struct lws_tokens *)(long)user);
-		/* you could return non-zero here and kill the connection */
-		break;
-
-	default:
-		break;
-	}
-
-	return 0;
-}
 
 
 /* list of supported protocols and callbacks */
@@ -364,14 +223,9 @@ static struct libwebsocket_protocols protocols[] = {
 		0			/* per_session_data_size */
 	},
 	{
-		"dumb-increment-protocol",
-		callback_dumb_increment,
-		sizeof(struct per_session_data__dumb_increment),
-	},
-	{
-		"lws-mirror-protocol",
-		callback_lws_mirror,
-		sizeof(struct per_session_data__lws_mirror)
+		"commotion-ws",
+		commotion_ws_callback,
+		sizeof(struct per_session_data__ws_client),
 	},
 	{
 		NULL, NULL, 0		/* End of list */
@@ -539,9 +393,7 @@ int main(int argc, char **argv)
 		 *
 		 * We take care of pre-and-post padding allocation.
 		 */
-
-		libwebsockets_broadcast(&protocols[PROTOCOL_DUMB_INCREMENT],
-					&buf[LWS_SEND_BUFFER_PRE_PADDING], 1);
+		//libwebsockets_broadcast(&protocols[PROTOCOL_COMMOTION_WS],&buf[LWS_SEND_BUFFER_PRE_PADDING], 1);
 	}
 
 #endif
@@ -550,3 +402,124 @@ int main(int argc, char **argv)
 
 	return 0;
 }
+
+
+
+
+
+
+#if 0
+
+/* lws-mirror_protocol */
+
+#define MAX_MESSAGE_QUEUE 64
+
+struct per_session_data__lws_mirror {
+	struct libwebsocket *wsi;
+	int ringbuffer_tail;
+};
+
+struct a_message {
+	void *payload;
+	size_t len;
+};
+
+static struct a_message ringbuffer[MAX_MESSAGE_QUEUE];
+static int ringbuffer_head;
+
+
+static int
+callback_lws_mirror(struct libwebsocket_context *context,
+			struct libwebsocket *wsi,
+			enum libwebsocket_callback_reasons reason,
+					       void *user, void *in, size_t len)
+{
+	int n;
+	struct per_session_data__lws_mirror *pss = user;
+
+	switch (reason) {
+
+	case LWS_CALLBACK_ESTABLISHED:
+		fprintf(stderr, "callback_lws_mirror: "
+						 "LWS_CALLBACK_ESTABLISHED\n");
+		pss->ringbuffer_tail = ringbuffer_head;
+		pss->wsi = wsi;
+		break;
+
+	case LWS_CALLBACK_SERVER_WRITEABLE:
+		if (close_testing)
+			break;
+		if (pss->ringbuffer_tail != ringbuffer_head) {
+
+			n = libwebsocket_write(wsi, (unsigned char *)
+				   ringbuffer[pss->ringbuffer_tail].payload +
+				   LWS_SEND_BUFFER_PRE_PADDING,
+				   ringbuffer[pss->ringbuffer_tail].len,
+								LWS_WRITE_TEXT);
+			if (n < 0) {
+				fprintf(stderr, "ERROR writing to socket");
+				exit(1);
+			}
+
+			if (pss->ringbuffer_tail == (MAX_MESSAGE_QUEUE - 1))
+				pss->ringbuffer_tail = 0;
+			else
+				pss->ringbuffer_tail++;
+
+			if (((ringbuffer_head - pss->ringbuffer_tail) %
+				  MAX_MESSAGE_QUEUE) < (MAX_MESSAGE_QUEUE - 15))
+				libwebsocket_rx_flow_control(wsi, 1);
+
+			libwebsocket_callback_on_writable(context, wsi);
+
+		}
+		break;
+
+	case LWS_CALLBACK_BROADCAST:
+		n = libwebsocket_write(wsi, in, len, LWS_WRITE_TEXT);
+		if (n < 0)
+			fprintf(stderr, "mirror write failed\n");
+		break;
+
+	case LWS_CALLBACK_RECEIVE:
+
+		if (ringbuffer[ringbuffer_head].payload)
+			free(ringbuffer[ringbuffer_head].payload);
+
+		ringbuffer[ringbuffer_head].payload =
+				malloc(LWS_SEND_BUFFER_PRE_PADDING + len +
+						  LWS_SEND_BUFFER_POST_PADDING);
+		ringbuffer[ringbuffer_head].len = len;
+		memcpy((char *)ringbuffer[ringbuffer_head].payload +
+					  LWS_SEND_BUFFER_PRE_PADDING, in, len);
+		if (ringbuffer_head == (MAX_MESSAGE_QUEUE - 1))
+			ringbuffer_head = 0;
+		else
+			ringbuffer_head++;
+
+		if (((ringbuffer_head - pss->ringbuffer_tail) %
+				  MAX_MESSAGE_QUEUE) > (MAX_MESSAGE_QUEUE - 10))
+			libwebsocket_rx_flow_control(wsi, 0);
+
+		libwebsocket_callback_on_writable_all_protocol(
+					       libwebsockets_get_protocol(wsi));
+		break;
+	/*
+	 * this just demonstrates how to use the protocol filter. If you won't
+	 * study and reject connections based on header content, you don't need
+	 * to handle this callback
+	 */
+
+	case LWS_CALLBACK_FILTER_PROTOCOL_CONNECTION:
+		dump_handshake_info((struct lws_tokens *)(long)user);
+		/* you could return non-zero here and kill the connection */
+		break;
+
+	default:
+		break;
+	}
+
+	return 0;
+}
+
+#endif
