744000cd29d0c382a999939ae35cc03abe6f3639
Adam Magaluk <adammagaluk@gmail.com>
2012-08-06 07:15:29 -0400

    More work on topology.

--- src/topology.c
+++ src/topology.c
@@ -12,9 +12,26 @@ void display_topology() {
     
 }
 
-int ap_exists(const Address ap){
-   json_t *mt = json_object_get(ap_root,"aps");
-   
+int ap_exists(const Address ap_addr) {
+    json_t *aps = json_object_get(ap_root, "aps");
+    if (!json_is_array(aps)) {
+        aps = json_array();
+    }
+    int i=0;
+    int size = json_array_size(aps);
+    
+    for(i=0;i<size;i++){
+        json_t* ap = json_array_get(aps, i);
+        if (json_is_object(aps)) {
+            json_t *addr = json_object_get(ap, "addr");
+            if (json_is_integer(addr)) {
+                if (json_integer_value(addr) == ap_addr) {
+                    return i;
+                }
+            }    
+        }
+    }
+    return -1;
 }
 
 int ap_add_node(const Address ap, const Address node) {
