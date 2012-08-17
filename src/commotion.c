b8b7a501fde6ce7ab74bb7efa16fce9ec34b1c76
Adam Magaluk <adammagaluk@gmail.com>
2012-08-17 09:53:51 -0400

    Refactored and commented commotion.c/h

--- src/commotion.c
+++ src/commotion.c
@@ -1,24 +1,71 @@
 
+// For getting ip address structures
 #include <netinet/in.h>
 
 #include "commotion.h"
+
 #define LOCAL_HOST_ADDR 0x7F000001
 
-// Check if t is valid msg type
-// Return 0 on success, all else is not valid.
+/**
+ * Main entry point for the protocol
+ *  - On connection it handles adding client to topology and updates topology
+ *  - On data parses protocol message
+ *  - On broadcast sends data to node
+ *  - On disconnect removes client from topology and updates topology. 
+ */
+extern int commotion_ws_callback(struct libwebsocket_context *context,
+        struct libwebsocket *wsi,
+        enum libwebsocket_callback_reasons reason,
+        void *user, void *in, size_t len) {
 
-inline int is_valid_msg_type(int t) {
-    switch (t) {
-        case COMMOTION_MSG_CLIENT_REGISTERED:
-        case COMMOTION_MSG_CLIENT_DISSCONNECTED:
-        case COMMOTION_MSG_FORWARD_MSG:
-        case COMMOTION_MSG_REQ_TOPOLOGY:
-            return 0;
+    // Get client data
+    struct per_session_data__ws_client *pss = user;
+    // Get fd of socket.
+    int socket=libwebsocket_get_socket_fd(wsi);
+    
+    switch (reason) {
+        case LWS_CALLBACK_ESTABLISHED:
+            // Get ip and hostname and store it in client data
+            libwebsockets_get_peer_addresses(socket, pss->client_name,
+                    sizeof (pss->client_name), pss->client_ip, sizeof (pss->client_ip));
+
+            // Get ip of client as int.
+            struct sockaddr_in antelope;
+            inet_aton(pss->client_ip, &antelope.sin_addr);
+            pss->addr.addr = htonl(antelope.sin_addr.s_addr);
+            pss->addr.id = socket;
+            
+            // Add node to topology
+            ap_add_node(getLocalAddress(), pss->addr, pss->client_name);
+            break;
+
+        case LWS_CALLBACK_BROADCAST:
+            if (libwebsocket_write(wsi,(unsigned char*)in, len, LWS_WRITE_TEXT) < 0) {
+                // @todo Handle error when write fails
+            }
+            break;
+        case LWS_CALLBACK_RECEIVE:
+            // Send to client data callback
+            return handle_client_data(context, wsi, user, in, len);
+            break;
+        case LWS_CALLBACK_FILTER_PROTOCOL_CONNECTION:
+            commotion_handshake_info((struct lws_tokens *) (long) user);
+            break;
+        case LWS_CALLBACK_CLOSED:
+            // Send to client disconnects
+            msg_client_disconnect(context, wsi, user);
+            break;
         default:
-            return -1;
+            break;
     }
+    return 0;
 }
 
+/**
+ * Handle all messages from the clients
+ *  - Parses the json and tests if it is a protocol,
+ *  - Calls each message type callback for specific commands
+ */
 static int handle_client_data(struct libwebsocket_context *context,
         struct libwebsocket *wsi, void *user, void *in, size_t len) {
     
@@ -36,7 +83,6 @@ static int handle_client_data(struct libwebsocket_context *context,
 
             //Get the message type
             const int msg_type = json_integer_value(mt);
-
             switch (msg_type) {
                 case COMMOTION_MSG_CLIENT_REGISTERED:
                     ret = msg_client_connect(context, wsi, user, root);
@@ -59,6 +105,11 @@ static int handle_client_data(struct libwebsocket_context *context,
     return ret;
 }
 
+/**
+ * Handle client_connect message, 
+ *   - Checks if data has the app parameter and if so adds to the topology
+ *     updates the topology if successful
+ */
 static int msg_client_connect(struct libwebsocket_context *context,
         struct libwebsocket *wsi, void *user, json_t *root) {
 
@@ -73,7 +124,7 @@ static int msg_client_connect(struct libwebsocket_context *context,
         return 0;
     }
 
-    apps = json_object_get(data, MSG_REGISTER_FIELD_PROTOCOLS);
+    apps = json_object_get(data, MSG_REGISTER_FIELD_APPS);
     if (!json_is_array(apps)) {
         fprintf(stderr, "error: missing protocols in message client connect\n");
         return 0;
@@ -86,7 +137,14 @@ static int msg_client_connect(struct libwebsocket_context *context,
     return 0;
 }
 
-int msg_client_disconnect(struct libwebsocket_context *context,
+/**
+ * Handles the libwebsockets closed event.
+ *   @todo should only handle message to close socket and close socket and call
+ * a seprate function that both the msg dissconect and socket close call.
+ * 
+ *  - Updates topology.
+ */
+static int msg_client_disconnect(struct libwebsocket_context *context,
         struct libwebsocket *wsi, void *user) {
 
     struct per_session_data__ws_client* pss = user;
@@ -98,29 +156,90 @@ int msg_client_disconnect(struct libwebsocket_context *context,
 }
 
 
+/**
+ * Handles forward message.
+ *  - Tries to forward message to the destination client if client exists in 
+ * topology.
+ * 
+ *  @todo - Possibly add error callback the messages the sending client when 
+ * dest is not found or fails to send.
+ */
+
+static int msg_forward_data(struct libwebsocket_context *context,
+        struct libwebsocket *wsi, void *user, json_t *root, void *in, size_t len) {
+    
+
+    json_t *data, *dst,*dst_id,*dst_ip;
+    dst = json_object_get(root, CWS_FIELD_DST);
+    if (!json_is_object(dst)) {
+        fprintf(stderr, "error: msg_forward_data: missing destination in message\n");
+        return 0;
+    }
+    
+    struct Address addr_st;
+    dst_ip = json_object_get(dst, "ip");
+    dst_id = json_object_get(dst, "id");
+    if (!json_is_integer(dst_ip) || !json_is_integer(dst_id)) {
+        fprintf(stderr, "error: msg_forward_data: destination object failed\n");
+        return 0;
+    }
+    addr_st.addr = json_integer_value(dst_ip);
+    addr_st.id = json_integer_value(dst_id);
+
+    struct Address host = return_ap_for_node(addr_st);
+    struct Address localHost = getLocalAddress();
+
+    data = json_object_get(root, CWS_FIELD_MSG_DATA);
+    if (!json_is_object(data)) {
+        fprintf(stderr, "error: missing data arg or not object\n");
+        return 0;
+    }
+    
+    // Append source address to message
+    struct per_session_data__ws_client *pss = user;
+    json_t* source=json_object();
+    addr_struct_to_json(pss->addr,source);
+    json_object_set_new(root,CWS_FIELD_SRC,source);
+    
+    // Handle for locally connected clients
+    if(compare_address(localHost, host) == 1) {
+        struct libwebsocket * nodews = wsi_from_fd(context, addr_st.id);
+        if (nodews != NULL) {
+            // Because we appended the new source address we have to remake packet
+            char *retData = json_dumps(root, JSON_COMPACT);
+            if (retData != NULL) {
+                unsigned char buf[LWS_SEND_BUFFER_PRE_PADDING + strlen(retData) + LWS_SEND_BUFFER_POST_PADDING];
+                unsigned char *p = &buf[LWS_SEND_BUFFER_PRE_PADDING];
+                int n;
+                n = sprintf((char *) p, "%s", retData);
+                if (libwebsocket_write(nodews, p, n, LWS_WRITE_TEXT) < 0) {
+                    fprintf(stderr, "error: failed to send..\n");
+                }
+                free(retData);
+            } else {
+                fprintf(stderr, "ERROR failed to dump json data\n");
+            }
+        }
+    } else {
+        //@todo Handle for remote hosts
+
+    }
+    return 0;
+}
 
 /**
- * Handle request topology
- * @param context
- * @param wsi
- * @param user
- * @param root
- * @return 
+ * Handles request topology
+ *  - Responds to node with topology
  */
 static int msg_req_topology(struct libwebsocket_context *context,
         struct libwebsocket *wsi, void *user, json_t *root) {
 
     struct per_session_data__ws_client* pss = user;
-    
-    
     json_t* msg = make_msg(COMMOTION_MSG_TOPOLOGY_UPDATE,"127.0.0.1",pss->client_ip,ap_root_topology());
-    
     char* retData = json_dumps(msg, JSON_COMPACT);
     if (retData != NULL) {
-
         unsigned char buf[LWS_SEND_BUFFER_PRE_PADDING + sizeof (retData) + LWS_SEND_BUFFER_POST_PADDING];
         unsigned char *p = &buf[LWS_SEND_BUFFER_PRE_PADDING];
-
         int n;
         n = sprintf((char *) p, "%s", retData);
         n = libwebsocket_write(wsi, p, n, LWS_WRITE_TEXT);
@@ -133,64 +252,60 @@ static int msg_req_topology(struct libwebsocket_context *context,
     }
     
     json_decref(msg);
-
     return 0;
 }
 
-int commotion_ws_callback(struct libwebsocket_context *context,
-        struct libwebsocket *wsi,
-        enum libwebsocket_callback_reasons reason,
-        void *user, void *in, size_t len) {
+
+/**
+ * Makes a json_t object with protocol, must free return value.
+ */
+static json_t* make_msg(int mt,  char* src,  char* dst, json_t* mdata) {
+    json_t* msg = json_object();
+    json_object_set_new(msg, CWS_FIELD_MSG_TYPE, json_integer(mt));
+
+    if (src != NULL)
+        json_object_set(msg, CWS_FIELD_SRC, json_string(src));
+    if (dst != NULL)
+        json_object_set(msg, CWS_FIELD_DST, json_string(dst));
+
+    if (mdata != NULL)
+        json_object_set(msg, CWS_FIELD_MSG_DATA, mdata);
     
-    int n;
-    unsigned char buf[LWS_SEND_BUFFER_PRE_PADDING + 512 + LWS_SEND_BUFFER_POST_PADDING];
-    unsigned char *p = &buf[LWS_SEND_BUFFER_PRE_PADDING];
-    struct per_session_data__ws_client *pss = user;
-    
-    int socket=libwebsocket_get_socket_fd(wsi);
-    switch (reason) {
+    return msg;
+}
 
-        case LWS_CALLBACK_ESTABLISHED:
-            
-            libwebsockets_get_peer_addresses(socket, pss->client_name,
-                    sizeof (pss->client_name), pss->client_ip, sizeof (pss->client_ip));
-
-            struct sockaddr_in antelope;
-            inet_aton(pss->client_ip, &antelope.sin_addr);
-            pss->addr.addr = htonl(antelope.sin_addr.s_addr);
-            pss->addr.id = socket;
-
-            int ret = ap_add_node(getLocalAddress(), pss->addr, pss->client_name);
-            break;
-
-        case LWS_CALLBACK_BROADCAST:
-            if (libwebsocket_write(wsi,(unsigned char*)in, len, LWS_WRITE_TEXT) < 0) {
-            }
-            break;
-        case LWS_CALLBACK_RECEIVE:
-            return handle_client_data(context, wsi, user, in, len);
-            break;
-        case LWS_CALLBACK_FILTER_PROTOCOL_CONNECTION:
-            commotion_handshake_info((struct lws_tokens *) (long) user);
-            break;
-        case LWS_CALLBACK_CLOSED:
-            msg_client_disconnect(context, wsi, user);
-            break;
-        default:
-            break;
+/**
+ *  Broadcasts topology to local clients.
+ *  @todo - Forward to all other aps.
+ */
+void update_topology() {
+    json_t* msg = make_msg(COMMOTION_MSG_TOPOLOGY_UPDATE, "127.0.0.1", "255.255.255.255", ap_root_topology());
+    char *retData = json_dumps(msg, JSON_COMPACT);
+    if (retData != NULL) {
+        unsigned char buf[LWS_SEND_BUFFER_PRE_PADDING + strlen(retData) + LWS_SEND_BUFFER_POST_PADDING];
+        unsigned char *p = &buf[LWS_SEND_BUFFER_PRE_PADDING];
+        int n;
+        n = sprintf((char *) p, "%s", retData);
+        n = libwebsockets_broadcast(&_socket_protocols[PROTOCOL_COMMOTION_WS], p, n);
+        if (n < 0) {
+            fprintf(stderr, "ERROR writing to socket\n");
+        }
+        free(retData);
+    } else {
+        fprintf(stderr, "ERROR failed to dump json data\n");
     }
-
-    return 0;
+    json_decref(msg);    
 }
 
 /*
  * this is just an example of parsing handshake headers, you don't need this
  * in your code unless you will filter allowing connections by the header
  * content
+ * 
+ * Taken from the demo application in libwebsockets, could be used to filter
+ * clients.
  */
-
-void
-commotion_handshake_info(struct lws_tokens *lwst) {
+void commotion_handshake_info(struct lws_tokens *lwst) {
     int n;
     static const char *token_names[WSI_TOKEN_COUNT] = {
         /*[WSI_TOKEN_GET_URI]		=*/ "GET URI",
@@ -227,115 +342,19 @@ commotion_handshake_info(struct lws_tokens *lwst) {
     }
 }
 
-static json_t* make_msg(int mt,  char* src,  char* dst, json_t* mdata) {
-    json_t* msg = json_object();
-    json_object_set_new(msg, CWS_FIELD_MSG_TYPE, json_integer(mt));
-
-    if (src != NULL)
-        json_object_set(msg, CWS_FIELD_SRC, json_string(src));
-    if (dst != NULL)
-        json_object_set(msg, CWS_FIELD_DST, json_string(dst));
-
-
-    if (mdata != NULL)
-        json_object_set(msg, CWS_FIELD_MSG_DATA, mdata);
-    
-    return msg;
-}
-
-void update_topology() {
-    json_t* msg = make_msg(COMMOTION_MSG_TOPOLOGY_UPDATE, "127.0.0.1", "255.255.255.255", ap_root_topology());
-    char *retData = json_dumps(msg, JSON_COMPACT);
-    if (retData != NULL) {
-        unsigned char buf[LWS_SEND_BUFFER_PRE_PADDING + strlen(retData) + LWS_SEND_BUFFER_POST_PADDING];
-        unsigned char *p = &buf[LWS_SEND_BUFFER_PRE_PADDING];
-        int n;
-        n = sprintf((char *) p, "%s", retData);
-        n = libwebsockets_broadcast(&_socket_protocols[PROTOCOL_COMMOTION_WS], p, n);
-        if (n < 0) {
-            fprintf(stderr, "ERROR writing to socket\n");
-        }
-        free(retData);
-    } else {
-        fprintf(stderr, "ERROR failed to dump json data\n");
+/**
+ * Initial callback for http connection before websocket protocol takes over.
+ *  Currently does nothing and allows the websocket to connect.
+ */
+extern int callback_http(struct libwebsocket_context *context,
+        struct libwebsocket *wsi,
+        enum libwebsocket_callback_reasons reason, void *user, void *in, size_t len) {
+    switch (reason) {
+        case LWS_CALLBACK_HTTP: break;
+        case LWS_CALLBACK_FILTER_NETWORK_CONNECTION:
+            break;
+        default:
+            break;
     }
-    json_decref(msg);    
-}
-
-static int msg_forward_data(struct libwebsocket_context *context,
-        struct libwebsocket *wsi, void *user, json_t *root, void *in, size_t len) {
-    
-
-    json_t *data, *dst,*dst_id,*dst_ip;
-    dst = json_object_get(root, CWS_FIELD_DST);
-    if (!json_is_object(dst)) {
-        fprintf(stderr, "error: msg_forward_data: missing destination in message\n");
-        return 0;
-    }
-    struct Address addr_st;
-    
-    dst_ip = json_object_get(dst, "ip");
-    dst_id = json_object_get(dst, "id");
-    if (!json_is_integer(dst_ip) || !json_is_integer(dst_id)) {
-        fprintf(stderr, "error: msg_forward_data: destination object failed\n");
-        return 0;
-    }
-    addr_st.addr = json_integer_value(dst_ip);
-    addr_st.id = json_integer_value(dst_id);
-
-    struct Address host = return_ap_for_node(addr_st);
-    struct Address localHost = getLocalAddress();
-
-    data = json_object_get(root, CWS_FIELD_MSG_DATA);
-    if (!json_is_object(data)) {
-        fprintf(stderr, "error: missing data arg or not object\n");
-        return 0;
-    }
-    
-    json_t* msgtype = json_object_get(data, "t");
-    json_t* msgdata = json_object_get(data, "d");
-    if(!json_is_string(msgtype)){
-        fprintf(stderr, "error: message data not string.\n");
-        return 0;
-    }
-    
-    struct per_session_data__ws_client *pss = user;
-    json_t* source=json_object();
-    addr_struct_to_json(pss->addr,source);
-    json_object_set_new(root,CWS_FIELD_SRC,source);
-    
-    // Handle for locally connected clients
-    if(compare_address(localHost, host) == 1) {
-
-        struct libwebsocket * nodews = wsi_from_fd(context, addr_st.id);
-        if (nodews != NULL) {
-
-            char *retData = json_dumps(root, JSON_COMPACT);
-            
-            if (retData != NULL) {
-                
-                unsigned char buf[LWS_SEND_BUFFER_PRE_PADDING + strlen(retData) + LWS_SEND_BUFFER_POST_PADDING];
-                unsigned char *p = &buf[LWS_SEND_BUFFER_PRE_PADDING];
-                int n;
-                n = sprintf((char *) p, "%s", retData);
-                if (libwebsocket_write(nodews, p, n, LWS_WRITE_TEXT) < 0) {
-                    fprintf(stderr, "error: failed to send..\n");
-                }
-                
-                free(retData);
-            } else {
-                fprintf(stderr, "ERROR failed to dump json data\n");
-            }
-          
-        }
-
-
-      }// Handle for remote hosts
-    else {
-
-    }
-
-
-    
     return 0;
 }
--- src/commotion.h
+++ src/commotion.h
@@ -15,194 +15,195 @@
 #include <string.h>
 #include <sys/time.h>
 
+// LibWebsockets api
 #include <libwebsockets.h>
-#include <private-libwebsockets.h>
+
+// Json parsing using libjansson
 #include <jansson.h>
 
+// Commotion topology managment.
 #include "topology.h"
 
 #ifdef	__cplusplus
 extern "C" {
 #endif
 
+// Base websocket protocol name
 #define COMMOTION_PROTOCOL_NAME "commotion-ws"
+// Message type
 #define CWS_FIELD_MSG_TYPE "mt"
-#define CWS_FIELD_MSG_DATA "d"
+// Source address, not required for all packets
 #define CWS_FIELD_SRC "src"
+// Destination address, not required for all packets.
 #define CWS_FIELD_DST "dst"
-#define MSG_REGISTER_FIELD_PROTOCOLS "p"
-    
-#define MAX_NUMBER_OF_PROTOCOLS 10
+//Data payload of packet
+#define CWS_FIELD_MSG_DATA "d"
+
+/**
+ * Commotion message types
+ */
+enum commotion_msg_type {
+    COMMOTION_MSG_CLIENT_REGISTERED, // After connection client sends this with apps
+    COMMOTION_MSG_CLIENT_DISSCONNECTED, // Force dissconnect
+    COMMOTION_MSG_FORWARD_MSG, // Foward message to destination
+    COMMOTION_MSG_REQ_TOPOLOGY, // Request the topology
+    COMMOTION_MSG_TOPOLOGY_UPDATE // Message sent when topology changes
+};
+
+// In COMMOTION_MSG_CLIENT_REGISTERED payload will have p:[String] for all apps
+#define MSG_REGISTER_FIELD_APPS "p"
+
+// For libwebsockets structure size
+#define MAX_NUMBER_OF_PROTOCOLS 3
+// For libwebsockets structure size
 #define MAX_PROTOCOl_NAME_SIZE 50
 
-    enum demo_protocols {
-        /* always first */
-        PROTOCOL_HTTP = 0,
+enum libwebsockets_protocols {
+    /* always first */
+    PROTOCOL_HTTP = 0,
 
-        PROTOCOL_COMMOTION_WS,
+    PROTOCOL_COMMOTION_WS,
 
-        /* always last */
-        DEMO_PROTOCOL_COUNT
-    };
-    
-      /*
-     * one of these is auto-created for each connection and a pointer to the
-     * appropriate instance is passed to the callback in the user parameter
-     *
-     * for this example protocol we use it to individualize the count for each
-     * connection.
-     */
-    struct per_session_data__ws_client {
-        char client_name[128];
-        char client_ip[128];
-        struct Address addr;
-    };
-    
-    
-    extern int callback_http(struct libwebsocket_context *context,
-		struct libwebsocket *wsi,
-		enum libwebsocket_callback_reasons reason, void *user, void *in, size_t len);
-    
-    extern int commotion_ws_callback(struct libwebsocket_context *context,
-            struct libwebsocket *wsi,
-            enum libwebsocket_callback_reasons reason,
-            void *user, void *in, size_t len);
-    
-      /* list of supported protocols and callbacks */
+    /* always last */
+    DEMO_PROTOCOL_COUNT
+};
 
-      extern struct libwebsocket_protocols _socket_protocols[] = {
-        /* first protocol must always be HTTP handler */
-
-        {
-            "http-only", /* name */
-            callback_http, /* callback */
-            0 /* per_session_data_size */
-        },
-        {
-            COMMOTION_PROTOCOL_NAME,
-            commotion_ws_callback,
-            sizeof (struct per_session_data__ws_client),
-        },
-        {
-            NULL, NULL, 0 /* End of list */
-        }
-    };
+/*
+ * one of these is auto-created for each connection and a pointer to the
+ * appropriate instance is passed to the callback in the user parameter
+ *
+ * for this example protocol we use it to individualize the count for each
+ * connection.
+ */
+struct per_session_data__ws_client {
+    char client_name[128]; // hostname of client
+    char client_ip[128]; // clients ip in text
+    struct Address addr; // Topology Address
+};
 
 
-/* this protocol server (always the first one) just knows how to do HTTP */
+
+
+/**
+ * Initial callback for http connection before websocket protocol takes over.
+ *  Currently does nothing and allows the websocket to connect.
+ */
 extern int callback_http(struct libwebsocket_context *context,
-		struct libwebsocket *wsi,
-		enum libwebsocket_callback_reasons reason, void *user, void *in, size_t len)
-{
-	switch (reason) {
-	case LWS_CALLBACK_HTTP: break;
-	case LWS_CALLBACK_FILTER_NETWORK_CONNECTION:
-		break;
-	default:
-		break;
-        }
+        struct libwebsocket *wsi,
+        enum libwebsocket_callback_reasons reason, void *user, void *in, size_t len);
 
-        return 0;
+/**
+ * Main entry point for the protocol
+ *  - On connection it handles adding client to topology and updates topology
+ *  - On data parses protocol message
+ *  - On broadcast sends data to node
+ *  - On disconnect removes client from topology and updates topology. 
+ */
+extern int commotion_ws_callback(struct libwebsocket_context *context,
+        struct libwebsocket *wsi,
+        enum libwebsocket_callback_reasons reason,
+        void *user, void *in, size_t len);
+
+/**
+ * Used by libwebsockets to define callbacks for each protocol
+ */
+extern struct libwebsocket_protocols _socket_protocols[] = {
+    /* first protocol must always be HTTP handler */
+    {
+        "http-only", /* name */
+        callback_http, /* callback for initial http connection*/
+        0 /* per_session_data_size */
+    },
+    {
+        COMMOTION_PROTOCOL_NAME, 
+        commotion_ws_callback, // Main callback for server
+        sizeof (struct per_session_data__ws_client),
+    },
+    {
+        NULL, NULL, 0 /* End of list */
     }
+};
 
 
+/**
+ * Temp function to get local address to server.
+ * @todo FIX ME it should be calling function to local ip.
+ */
+struct Address getLocalAddress() {
+    struct Address a;
+    a.addr = LOCAL_HOST_ADDR;
+    a.id = 0;
+    return a;
+}
 
 
-
-    
-    enum commotion_msg_type {
-        COMMOTION_MSG_CLIENT_REGISTERED,
-        COMMOTION_MSG_CLIENT_DISSCONNECTED,
-        COMMOTION_MSG_FORWARD_MSG,
-        COMMOTION_MSG_REQ_TOPOLOGY,
-        COMMOTION_MSG_TOPOLOGY_UPDATE
-    };
-
-    
-    // Check if t is valid msg type
-    // Return 0 on success, all else is not valid.
-    inline int is_valid_msg_type(int t);
-
-  
-    struct Address getLocalAddress(){
-        struct Address a;
-        a.addr = LOCAL_HOST_ADDR;
-        a.id = 0;
-        return a;
-    }
-    
-    /**
-     * Main Callback for commotion and libwebsockets
-     * @param context
-     * @param wsi
-     * @param reason
-     * @param user
-     * @param in
-     * @param len
-     * @return 
-     */
-    extern int commotion_ws_callback(struct libwebsocket_context *context,
-            struct libwebsocket *wsi,
-            enum libwebsocket_callback_reasons reason,
-            void *user, void *in, size_t len);
+/*
+ * this is just an example of parsing handshake headers, you don't need this
+ * in your code unless you will filter allowing connections by the header
+ * content
+ * 
+ * Taken from the demo application in libwebsockets, could be used to filter
+ * clients.
+ */
+extern void commotion_handshake_info(struct lws_tokens *lwst);
 
 
-    /*
-     * this is just an example of parsing handshake headers, you don't need this
-     * in your code unless you will filter allowing connections by the header
-     * content
-     */
-
-    extern void commotion_handshake_info(struct lws_tokens *lwst);
-
-
-    /**
-     * Handle all client data
-     * @param context
-     * @param wsi
-     * @param user
-     * @param in
-     * @param len
-     * @return 
-     */
-    static int handle_client_data(struct libwebsocket_context *context,
-            struct libwebsocket *wsi, void *user, void *in, size_t len);
+/**
+ * Handle all messages from the clients
+ *  - Parses the json and tests if it is a protocol,
+ *  - Calls each message type callback for specific commands
+ */
+static int handle_client_data(struct libwebsocket_context *context,
+        struct libwebsocket *wsi, void *user, void *in, size_t len);
 
 
-    /**
-     * Handle client_connect
-     * @param context
-     * @param wsi
-     * @param user
-     * @param root
-     * @return 
-     */
-    static int msg_client_connect(struct libwebsocket_context *context,
-            struct libwebsocket *wsi, void *user, json_t *root);
-    
-    extern int msg_client_disconnect(struct libwebsocket_context *context,
-            struct libwebsocket *wsi, void *user);
-    
-    static int msg_forward_data(struct libwebsocket_context *context,
+/**
+ * Handle client_connect message, 
+ *   - Checks if data has the app parameter and if so adds to the topology
+ *     updates the topology if successful
+ */
+static int msg_client_connect(struct libwebsocket_context *context,
+        struct libwebsocket *wsi, void *user, json_t *root);
+
+/**
+ * Handles the libwebsockets closed event.
+ *   @todo should only handle message to close socket and close socket and call
+ * a seprate function that both the msg dissconect and socket close call.
+ * 
+ *  - Updates topology.
+ */
+static int msg_client_disconnect(struct libwebsocket_context *context,
+        struct libwebsocket *wsi, void *user);
+
+/**
+ * Handles forward message.
+ *  - Tries to forward message to the destination client if client exists in 
+ * topology.
+ * 
+ *  @todo - Possibly add error callback the messages the sending client when 
+ * dest is not found or fails to send.
+ */
+static int msg_forward_data(struct libwebsocket_context *context,
         struct libwebsocket *wsi, void *user, json_t *root, void *in, size_t len);
 
-    /**
-     * Handle client_connect
-     * @param context
-     * @param wsi
-     * @param user
-     * @param root
-     * @return 
-     */
-    static int msg_req_topology(struct libwebsocket_context *context,
-            struct libwebsocket *wsi, void *user, json_t *root);
+/**
+ * Handles request topology
+ *  - Responds to node with topology
+ */
+static int msg_req_topology(struct libwebsocket_context *context,
+        struct libwebsocket *wsi, void *user, json_t *root);
 
+/**
+ * Makes a json_t object with protocol, must free return value.
+ */
+static json_t* make_msg(int mt, char* src, char* dst, json_t* mdata);
 
-    static json_t* make_msg(int mt, char* src, char* dst,json_t* mdata);
-    
-    extern void update_topology();
-    
-    
+/**
+ *  Broadcasts topology to local clients.
+ *  @todo - Forward to all other aps.
+ */
+extern void update_topology();
+
 #ifdef	__cplusplus
 }
 #endif
