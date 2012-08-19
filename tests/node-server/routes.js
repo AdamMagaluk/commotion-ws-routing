ca195581490d973de346c849c77683e24d2a2957
Adam Magaluk <adammagaluk@gmail.com>
2012-08-18 23:37:02 -0400

    Added luci test page to node server for testing css styles

--- tests/node-server/routes.js
+++ tests/node-server/routes.js
@@ -3,6 +3,10 @@ module.exports = function(app,settings){
     app.all('/', function(req, res) {
         res.render('index');
     });
+    
+    app.all('/luci', function(req, res) {
+        res.render('luci');
+    });
 
 // If all fails, hit em with the 404
 app.all('*', function(req, res){
