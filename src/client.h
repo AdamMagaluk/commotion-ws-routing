294961898c4e950fc5873d20ce85dbe1db3f4b25
Adam Magaluk <adammagaluk@gmail.com>
2012-09-23 13:27:22 -0400

    AP protocol

--- src/client.h
+++ src/client.h
@@ -33,15 +33,40 @@ extern "C" {
     extern int client_callback_http(struct libwebsocket_context *context,
             struct libwebsocket *wsi,
             enum libwebsocket_callback_reasons reason, void *user, void *in, size_t len) {
+
+        switch (reason) {
+            case LWS_CALLBACK_HTTP: break;
+            case LWS_CALLBACK_FILTER_NETWORK_CONNECTION:
+                break;
+            default:
+                break;
+        }
         return 0;
     }
-
-    /**
+/**
      */
     extern int client_ws_callback(struct libwebsocket_context *context,
             struct libwebsocket *wsi,
             enum libwebsocket_callback_reasons reason,
             void *user, void *in, size_t len) {
+
+        switch (reason) {
+            case LWS_CALLBACK_ESTABLISHED:
+                break;
+
+            case LWS_CALLBACK_BROADCAST:
+                if (libwebsocket_write(wsi, (unsigned char*) in, len, LWS_WRITE_TEXT) < 0) {
+                }
+                break;
+            case LWS_CALLBACK_RECEIVE:
+                break;
+            case LWS_CALLBACK_FILTER_PROTOCOL_CONNECTION:
+                break;
+            case LWS_CALLBACK_CLOSED:
+                break;
+            default:
+                break;
+        }
         return 0;
     }
 
@@ -72,7 +97,7 @@ extern "C" {
         int port = 7681;
         int use_ssl = 0;
         struct libwebsocket_context *context;
-        struct libwebsocket *wsi_dumb;
+        struct libwebsocket *websocket;
         int ietf_version = -1;
 
         context = libwebsocket_create_context(CONTEXT_PORT_NO_LISTEN, NULL,
@@ -85,38 +110,32 @@ extern "C" {
 
         const char *address = addr_to_string(ntohl(addr));
         fprintf(stderr, "Trying to connect to %s\n", address);
-        wsi_dumb = libwebsocket_client_connect(context, address, port, use_ssl,
+        websocket = libwebsocket_client_connect(context, address, port, use_ssl,
                 "/", address, address,
                 COMMOTION_AP_PROTOCOL_NAME, ietf_version);
 
-        if (wsi_dumb == NULL) {
+        if (websocket == NULL) {
             fprintf(stderr, "libwebsocket dumb connect failed\n");
             return -1;
         }
 
         fprintf(stderr, "Websocket to %s connections opened\n", address);
-
-
-
-        /*
+        
             char *retData = json_dumps(msg, JSON_COMPACT);
             if (retData != NULL) {
                 unsigned char buf[LWS_SEND_BUFFER_PRE_PADDING + strlen(retData) + LWS_SEND_BUFFER_POST_PADDING];
                 unsigned char *p = &buf[LWS_SEND_BUFFER_PRE_PADDING];
                 int n;
                 n = sprintf((char *) p, "%s", retData);
-
-                n = libwebsockets_broadcast(&_socket_protocols[PROTOCOL_COMMOTION_WS], p, n);
-                if (n < 0) {
-                    fprintf(stderr, "ERROR writing to socket\n");
+                if (libwebsocket_write(websocket, p, n, LWS_WRITE_TEXT) < 0) {
+                    fprintf(stderr, "error: failed to send..\n");
                 }
-
                 free(retData);
             } else {
                 fprintf(stderr, "ERROR failed to dump json data\n");
             }
             json_decref(msg);
-         */
+         
     }
 
 #ifdef	__cplusplus
